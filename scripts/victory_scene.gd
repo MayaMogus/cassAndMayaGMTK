fa6f96941467efe8b5ef110f7319b6531fa760d3
MayaMogus <damienmahony.dm@gmail.com>
2025-08-03 16:21:30 +0100

    Victory Screen Stuff

--- scripts/victory_scene.gd
+++ scripts/victory_scene.gd
@@ -3,9 +3,19 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
+	var splitsText : String
+	var index := 0
+	for stageInfo in Info.stage_info:
+		splitsText += str(Info.stage_names[index], ' : ', stageInfo.current_time, ' - DEATHS : ', stageInfo.current_deaths, '\n')
+		index += 1
+	$ScrollContainer/Splits.text = splitsText
+	$Time.text = GameTimer.GetGameTime()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$Sprite2D2.modulate.a = lerp($Player/Sprite2D2.modulate.a, 0, delta * 20)
+	$FadeWhite.modulate.a = lerp($FadeWhite.modulate.a, 0.0, delta)
+	
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_packed(preload("res://scenes/menus/main_menu.tscn"))
+	Info.current_stage = 0
